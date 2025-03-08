1dd8ba225dea6d604ca83b7dbe0d9b6854905997
Raksha <rakshac29@gmail.com>
2025-03-08 16:50:45 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <h1 className="text-4xl font-bold  text-blue-600">Hello, Tailwind with Vite!</h1>
+    <h1 className='color'></h1>
+  </div>
+  )
+}
+
+export default App
